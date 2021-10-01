e2b8954d1bc69e2a8ad936250508ad1af4e58a53
Jaolmos <jose.a.olmosmartinez@gmail.com>
2021-10-01 07:08:16 +0200

    add usermanager

--- users/applications/users/models.py
+++ users/applications/users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.db.models.fields import EmailField
+
+from .managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
     
@@ -12,11 +13,13 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     username = models.CharField('Username', max_length=20, unique=True)
     email = models.EmailField()
-    first_name = models.CharField('First name', max_length=30)
-    last_name = models.CharField('First name', max_length=30)
+    first_name = models.CharField('First name', max_length=30, blank=True)
+    last_name = models.CharField('First name', max_length=30, blank=True)
     gender = models.CharField('Gender', max_length=1, choices=GENDER_CHOICES, blank=True)
 
     USERNAME_FIELD = 'username'
+
+    object = UserManager()
 
     def get_short_name(self):
         return self.username
