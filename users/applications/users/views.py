a4ced255496ac317e670cad28ae4445296931071
Jaolmos <jose.a.olmosmartinez@gmail.com>
2021-10-02 18:27:44 +0200

    add function form_valid

--- users/applications/users/views.py
+++ users/applications/users/views.py
@@ -4,9 +4,23 @@ from django.views.generic.edit import FormView
 
 from .forms import UserRegisterForm
 
+from .models import User
+
 
 class UserRegisterView(FormView):
     template_name = 'users/register.html'
     form_class = UserRegisterForm
     success_url = '/'
 
+    def form_valid(self, form):
+        
+        User.objects.create_user(
+            form.cleaned_data['username'],
+            form.cleaned_data['email'],
+            form.cleaned_data['password1'],
+            first_name = form.cleaned_data['first_name'],
+            last_name = form.cleaned_data['last_name'],
+            gender = form.cleaned_data['gender'],
+        )
+
+        return super(UserRegisterForm,self).form_valid(form)
