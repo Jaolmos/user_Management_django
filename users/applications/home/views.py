8b0f590be873fa00b47ff84d1b53448fb2f9c15f
Jaolmos <jose.a.olmosmartinez@gmail.com>
2021-10-06 06:34:51 +0200

    Add Login Required Mixin

--- users/applications/home/views.py
+++ users/applications/home/views.py
@@ -1,7 +1,13 @@
+import datetime
+
 from django.views.generic import TemplateView 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
    
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "home/index.html"
+    login_url = reverse_lazy('users_app:user_login')
+
 
 
